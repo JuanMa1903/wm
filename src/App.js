4bdd4b5c345d2694bf7175eb26d21ac77e807749
Juan <juands1903@gmail.com>
2022-04-02 01:45:39 -0500

    Actualizacion 02-04-2022

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import  React, {useEffect} from "react";
 
 function App() {
+
+  const url = "https://api.github.com/repos/watermelontools/wm-extension";
+  const fetchApi = async () => {
+    const Response = await fetch(url);
+    const ResponseJson = await Response.json();
+    const ForksJson = ResponseJson.forks;
+    const WatchersJson = ResponseJson.watchers;
+    console.log(ForksJson);
+    console.log(WatchersJson);
+
+  };
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="App">
+      <div class="container">
+        <div class="jumbotron jumbotron-fluid mb-3 pt-0 pb-0 bg-lightblue position-relative">
+          <div class="col-md-6 pt-6 pb-6 align-self-center">
+            <h4 class="mb-3">Forks:</h4>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="jumbotron jumbotron-fluid mb-3 pt-0 pb-0 bg-lightblue position-relative">
+          <div class="col-md-6 pt-6 pb-6 align-self-center">
+            <h4 class="mb-3">Watchers:</h4>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
